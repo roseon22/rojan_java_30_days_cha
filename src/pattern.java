abca19fdf4bae4ad72461d6ec82ffc83e0e3dc69
Rojan Uprety <rojan.uprety@gmail.com>
2023-02-04 19:34:35 +0545

    Hi

--- src/pattern.java
+++ src/pattern.java
@@ -7,7 +7,7 @@ public class pattern{
                 System.out.print("*");
 
             }
-            System.out.println(" ");
+            System.out.println(" Hellow Word!");
         }
     }
 
