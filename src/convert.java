8198b83a7c51e8e6b9323514e04fee8e68c87d7c
Rojan Uprety <rojan.uprety@gmail.com>
2023-02-05 14:02:07 +0545

    convert

---
+++ src/convert.java
@@ -0,0 +1,20 @@
+public class convert {
+    public static void main(String[] args) {
+//        String s = "100";
+//
+//        int i = Integer.parseInt(s);
+//
+//        System.out.println(s + 100);
+//
+//        System.out.println(i + 100);
+
+        Integer i = 100;
+        String s = String.valueOf(i);
+
+        System.out.println(s + 100);
+
+
+
+
+    }
+}
