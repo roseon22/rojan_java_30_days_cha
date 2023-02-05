ccf27930daddc2cc995336032c48ab9d2c9175cf
Rojan Uprety <rojan.uprety@gmail.com>
2023-02-05 21:51:53 +0545

    skip numbeer using continue

---
+++ src/fo.java
@@ -0,0 +1,11 @@
+public class fo {
+    public static void main(String[] args) {
+        for (int i=1; i<=100; i++) {
+            if( i == 41) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
